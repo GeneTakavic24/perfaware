74339f29042953abbc9aea3dc349942a0696bf74
gene <genetakavic380@gmail.com>
2025-02-23 18:22:26 +0400

    Add direct access support

--- src/decode_multiple_8086/decode.go
+++ src/decode_multiple_8086/decode.go
@@ -7,7 +7,10 @@ import (
 
 const mov_inst = 0b100010
 const immediate_to_reg_mov = 0b1011
+const memory_to_accumulator = 0b1010000
+const accumulator_to_memory = 0b1010001
 const mov_mnemonic = "mov"
+const ax = "ax"
 const reg_mask byte = 0b111
 
 var fields = [16]string{
@@ -20,13 +23,14 @@ var effective_addresses = [8]string{
 }
 
 func decode(bytes *[]byte) (instruction string, consumed int) {
+
 	switch {
-
 	case (*bytes)[0]>>2 == mov_inst:
-		instruction, consumed = decodeMov(bytes, reg_mask)
+		instruction, consumed = decodeMov(bytes)
 	case (*bytes)[0]>>4 == immediate_to_reg_mov:
-		instruction, consumed = decodeImmediateMov(bytes, reg_mask)
-
+		instruction, consumed = decodeImmediateMov(bytes)
+	case (*bytes)[0]>>1 == memory_to_accumulator || (*bytes)[0]>>1 == accumulator_to_memory:
+		instruction, consumed = decodeAccumulator(bytes)
 	default:
 		panic("Unknown instruction")
 	}
@@ -34,16 +38,16 @@ func decode(bytes *[]byte) (instruction string, consumed int) {
 	return
 }
 
-func decodeMov(bytes *[]byte, regMask byte) (instruction string, consumed int) {
+func decodeMov(bytes *[]byte) (instruction string, consumed int) {
 	builder := strings.Builder{}
 	builder.WriteString(mov_mnemonic + " ")
 
 	firstByte := (*bytes)[0]
 	w := firstByte & 1
 	d := (firstByte >> 1) & 1
-	reg := ((*bytes)[1] >> 3) & regMask
+	reg := ((*bytes)[1] >> 3) & reg_mask
 	mod := ((*bytes)[1] >> 6)
-	rm := ((*bytes)[1]) & regMask
+	rm := ((*bytes)[1]) & reg_mask
 
 	var rm_decoded string
 
@@ -56,13 +60,14 @@ func decodeMov(bytes *[]byte, regMask byte) (instruction string, consumed int) {
 	} else { // memory
 		rm_decoded = decodeEffectiveAddress(rm)
 
+		directAccess := mod == 0 && rm_decoded == "bp"
+
 		var offset int
 
-		switch mod {
-		case 1:
+		if mod == 1 {
 			offset = int((*bytes)[2])
 			consumed = 3
-		case 0b10:
+		} else if mod == 0b10 || directAccess {
 			offset = (int((*bytes)[3]) << 8) | int((*bytes)[2])
 			consumed = 4
 		}
@@ -70,7 +75,11 @@ func decodeMov(bytes *[]byte, regMask byte) (instruction string, consumed int) {
 		if offset == 0 {
 			rm_decoded = fmt.Sprintf("[%s]", rm_decoded)
 		} else {
-			rm_decoded = fmt.Sprintf("[%s + %d]", rm_decoded, offset)
+			if directAccess {
+				rm_decoded = fmt.Sprintf("[%d]", offset)
+			} else {
+				rm_decoded = fmt.Sprintf("[%s + %d]", rm_decoded, offset)
+			}
 		}
 	}
 
@@ -83,13 +92,13 @@ func decodeMov(bytes *[]byte, regMask byte) (instruction string, consumed int) {
 	return builder.String(), consumed
 }
 
-func decodeImmediateMov(bytes *[]byte, regMask byte) (instruction string, consumed int) {
+func decodeImmediateMov(bytes *[]byte) (instruction string, consumed int) {
 	builder := strings.Builder{}
 	builder.WriteString(fmt.Sprintf("%s ", mov_mnemonic))
 
 	firstByte := (*bytes)[0]
 	w := (firstByte >> 3) & 1
-	reg := firstByte & regMask
+	reg := firstByte & reg_mask
 
 	builder.WriteString(fmt.Sprintf("%s, ", decodeRegister(reg, &w)))
 
@@ -125,6 +134,31 @@ func decodeEffectiveAddress(rm byte) string {
 	}
 
 	panic("Unknown register")
+}
+
+func decodeAccumulator(bytes *[]byte) (instruction string, consumed int) {
+	builder := strings.Builder{}
+	consumed = 2
+	firstByte := (*bytes)[0]
+
+	w := firstByte & 1
+	data := int((*bytes)[1])
+
+	if w == 1 {
+		data2 := int((*bytes)[2]) << 8
+		data = data2 | data
+		consumed++
+	}
+
+	dataStr := fmt.Sprintf("[%d]", data)
+
+	if (firstByte>>1)&1 == 0 {
+		writeOperands(&builder, ax, dataStr)
+	} else {
+		writeOperands(&builder, dataStr, ax)
+	}
+
+	return builder.String(), consumed
 }
 
 func writeOperands(builder *strings.Builder, dest, src string) {
